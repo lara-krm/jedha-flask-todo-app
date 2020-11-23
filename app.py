f67a3cf8c2e9bc9db71969a8891eefd6246608b2
Lara <lara.kremer.pro@gmail.com>
2020-11-23 15:08:29 +0100

    correction pour mac

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 DATABASE = "./todo.db"
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////" + DATABASE
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + DATABASE
 db = SQLAlchemy(app)
 
 
